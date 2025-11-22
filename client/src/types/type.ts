e95adabe4981f08c91b9185fe1b9d3131141c56a
theeliteclub2k <vanjregautham@gmail.com>
2025-11-22 20:52:14 +0530

    feat: upload file to Pinata

--- client/src/types/type.ts
+++ client/src/types/type.ts
@@ -113,3 +113,15 @@ export const statusColors: Record<JobStatus, string> = {
   Submitted: "bg-yellow-600/20 text-yellow-400 border-yellow-600/50",
   Completed: "bg-blue-600/20 text-blue-400 border-blue-600/50",
 };
+
+export type pinataUploadFileAPIrepsonseType = {
+  data: {
+    id: string;
+    name: string;
+    cid: string;
+    size: number;
+    number_of_files: number;
+    mime_type: string;
+    group_id: null;
+  };
+};
